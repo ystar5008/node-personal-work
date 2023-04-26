ceb1058677774558bf424d6c710a84fe46c82984
ystar5008@naver.com <ystar5008@naver.com>
2023-04-26 10:26:07 +0900

    feat : catch 에러 수정

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -14,11 +14,18 @@ module.exports = async (req, res, next) => {
         const { userId } = jwt.verify(authToken, "customized secret key")
         const user = await Users.findOne({ where: { userId } })
         res.locals.user = user;
+
         next();
     } catch (err) {
         //403 Cookie가 비정상적이거나 만료된 경우
-        console.error(err);
+        if (err instanceof jwt.TokenExpiredError) {
+            return res.status(401).json({ errorMessage: "인증 정보가 만료되었습니다." });
+        }
+        if (err instanceof jwt.JsonWebTokenError) {
+            // 401: 토큰이 조작된 경우
+            return res.status(401).json({ errorMessage: "인증 정보가 올바르지 않습니다." });
+        }
         res.status(403).json({ "errorMessage": "로그인이 필요한 기능입니다." });
-        return;
+
     }
 };
