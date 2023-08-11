b8317900b17fb8ae245d355b051a01e12dc9fa53
index.ys <96641210+ystar5008@users.noreply.github.com>
2023-08-11 15:43:53 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.js
@@ -0,0 +1,10 @@
+function solution(my_str, n) {
+    let answer = [];
+
+    for (let i = 0; i < my_str.length; i += n) {
+        if (i % n === 0) {
+            answer.push(my_str.substr(i, n))
+        }
+    }
+    return answer;
+}
